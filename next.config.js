83ebc82fff1ead4eb573a7f5eca223f54eb04b28
vikeshdass <vikeshdass@gmail.com>
2025-08-03 01:49:19 +0400

    fix: Configure image optimization and update Next.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/KeepMeOut.github.io/' : '',
   images: {
-    unoptimized: true
-  }
+    loader: 'custom',
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  env: {
+    nextImageExportOptimizer_imageFolderPath: 'public/assets',
+    nextImageExportOptimizer_exportFolderPath: 'out',
+    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_storePicturesInWEBP: true,
+    nextImageExportOptimizer_exportFolderName: 'nextImageExportOptimizer',
+    nextImageExportOptimizer_generateAndUseBlurImages: true,
+  },
 }
 
 module.exports = nextConfig
