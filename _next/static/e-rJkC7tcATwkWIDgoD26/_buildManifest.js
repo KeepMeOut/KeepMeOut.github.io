04dd46c83231d37077e5a9f51421f549802acd90
shadygoneinsane <shadygoneinsane@users.noreply.github.com>
2025-08-03 09:04:02 +0000

    deploy: 10e2a4d03282ab2802dfcb8fd6a1b7adcbd1e35c

---
+++ _next/static/e-rJkC7tcATwkWIDgoD26/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-3a2f3aa75d3e20ad.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
